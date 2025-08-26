87b3477127c43420fe372bd42b820712d74d1658
Hanif Daffa N <mail.hanifdaffan@gmail.com>
2025-08-26 16:16:55 +0700

    fix: service account type

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -3,9 +3,9 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  projectId: process.env.FIREBASE_PROJECT_ID || "",
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || "",
 };
 
 function getFirebaseAdminApp() {
